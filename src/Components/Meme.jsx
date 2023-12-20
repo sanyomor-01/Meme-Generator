f6c0ec9b7320d9341daa0a440329ea0c00490a4e
sanyomor-01 <sanyomormichael@gmail.com>
2023-12-20 16:19:03 +0000

    structure modified

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,54 +1,59 @@
 import React from "react"
-import memesData from "../memesData"
+import memesData from "../memesData.jsx"
 
 export default function Meme() {
-
+    /**
+     * Challenge: 
+     * 1. Set up the text inputs to save to
+     *    the `topText` and `bottomText` state variables.
+     * 2. Replace the hard-coded text on the image with
+     *    the text being saved to state.
+     */
+    
     const [meme, setMeme] = React.useState({
-        topText    : "",
-        bottomText : "",
-        randomImage : "https://i.imgflip.com/1g8my4.jpg"
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
-
-    const [allMemeImage, setAllMemeImage] = React.useState(memesData)
- 
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
     
     function getMemeImage() {
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         const url = memesArray[randomNumber].url
-        setMeme((prevMeme)=> ({
-            ...prevMeme, randomImage : url
-        }) )
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
         
     }
     
     return (
         <main>
-            <form>
-                <div>
-                    <label className="form--label">Top text</label>
-
-                    <input 
-                        type="text"
-                        placeholder="Top text"
-                        className="form--input"
-                    />
-                </div>
-                <div>
-                    <label className="form--label">Top text</label>
-                    <input 
-                        type="text"
-                        placeholder="Bottom text"
-                        className="form--input"
-                    />
-                </div>
-                <button
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
-            <img src={meme.randomImage} className="meme--image" />
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">One does not simply</h2>
+                <h2 className="meme--text bottom">Walk into Mordor</h2>
+            </div>
         </main>
     )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react"
 import logo from '../assets/troll-face.png'
 
-export default function Navbar() {
+export default function Header() {
     return (
-        <div>
-          <header>
-              <img  className='troll-face' src= {logo} alt="troll face" />
-              <h2 className='brand--text' >Meme Generator</h2>
-             <h4 className='project--title' >  React Course - Project 3 </h4>
-          </header>
-        </div>
+        <header className="header">
+            <img 
+                src={logo}
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
     )
 }
