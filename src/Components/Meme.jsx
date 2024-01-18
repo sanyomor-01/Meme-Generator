879205a1330353667ca0c16f4386d0a32abfa581
sanyomor-01 <sanyomormichael@gmail.com>
2024-01-18 19:37:12 +0000

    meme data fetched from an api

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import memesData from "../memesData.jsx"
 
 export default function Meme() {
 
@@ -9,13 +8,17 @@ export default function Meme() {
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
-    
-    
+
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        fetch(`https://api.imgflip.com/get_memes`)
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() *allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
@@ -23,7 +26,7 @@ export default function Meme() {
         
     }
 
-    {/* fuction to handle change*/}
+    /* fuction to handle change*/
     function handleChange(event) {
         const {name, value } = event.target
 
@@ -57,7 +60,7 @@ export default function Meme() {
                 </button>
             </div>
             <div className="meme">
-                <img src={meme.randomImage} className="meme--image" />
+                <img src={meme.randomImage} className="meme--image" alt="" />
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
